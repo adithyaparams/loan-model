7828ded135f848dcdb70a7423a4d9b124474f6ec
adithya <adithyaparams@gmail.com>
2019-05-19 18:47:25 -0400

    Try redirect to https

--- app/routes.py
+++ app/routes.py
@@ -48,6 +48,13 @@ def career_list():
         careers.append(minor)
     return careers
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 @app.route('/')
 @app.route('/index')
 def index():
